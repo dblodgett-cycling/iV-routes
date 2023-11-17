2b942bff792368faa823d54361866df29a6c60c7
David Blodgett <dblodgett.h2o@gmail.com>
2023-11-16 20:49:04 -0600

    better svg

--- iv_routes.R
+++ iv_routes.R
@@ -26,12 +26,51 @@ make_outputs <- function(x, routes) {
   
   sf::write_sf(d, out)
   
+  d$elevation <- d$elevation - 35
+  
   out <- file.path("svg", gsub("gpx", "svg", x))
   
   svglite::svglite(out)
-  par(mar = c(0,0,0,0))
-  plot(d$distance, d$elevation, col = NA)
+  
+  par(mar = c(1,3,0,0))
+  
+  if(max(d$elevation) > 200) {
+    range <- c(0, 250)
+  } else if(max(d$elevation) > 150) {
+    range <- c(0, 200)
+  } else if(max(d$elevation) > 100) {
+    range <- c(0, 150)
+  } else {
+    range <- c(0, 100)
+  }
+  
+  plot(c(min(d$distance), max(d$distance)), range, col = NA, axes = FALSE)
+  
   lines(d$distance, d$elevation)
+
+  abline(0, 0, col = "black")
+  
+  text(0, 0, "0m", pos = 2, offset = 2, xpd = NA)
+    
+  abline(50, 0, col = "lightgrey", lty = "dashed")
+  
+  text(0, 50, "50m", pos = 2, offset = 1.5, xpd = NA)
+  
+  if(max(range) > 100) {
+    abline(100, 0, col = "lightgrey", lty = "dashed")
+    text(0, 100, "100m", pos = 2, offset = 1.5, xpd = NA)
+  }
+  
+  if(max(range) > 150) {
+    abline(150, 0, col = "lightgrey", lty = "dashed")
+    text(0, 150, "150m", pos = 2, offset = 1.5, xpd = NA)
+  }
+  
+  if(max(range) > 200) {
+    abline(200, 0, col = "lightgrey", lty = "dashed")
+    text(0, 200, "200m", pos = 2, offset = 1.5, xpd = NA)
+  }
+  
   dev.off()
 }
 
